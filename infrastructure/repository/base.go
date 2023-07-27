463822354ad341127f23df361fb6eccfdd9ef282
Alima <aliali.ali1378@yahoo.com>
2023-07-28 01:08:34 +0330

    create database connection

--- infrastructure/repository/base.go
+++ infrastructure/repository/base.go
@@ -1,1 +1,31 @@
 package repository
+
+import (
+	"github.com/go-redis/redis/v8"
+	"gorm.io/gorm"
+	"sync"
+)
+
+type RedisDB struct {
+	Client *redis.Client
+}
+
+type PGRepository struct {
+	DB *gorm.DB
+}
+
+func RedisInit() *RedisDB {
+	o := sync.Once{}
+	var db *RedisDB
+	o.Do(func() {
+		db = &RedisDB{
+			Client: createRedisConnection(),
+		}
+	})
+	return db
+}
+
+func NewGormDatabase() *PGRepository {
+	db, _ := GormInit()
+	return &PGRepository{DB: db}
+}
--- infrastructure/repository/gorm.go
+++ infrastructure/repository/gorm.go
@@ -1,1 +1,32 @@
 package repository
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+type Postgres struct {
+	db *gorm.DB
+}
+
+func GormInit() (*gorm.DB, error) {
+	host := os.Getenv("PG_HOST")
+	user := os.Getenv("PG_USER")
+	password := os.Getenv("PG_PASSWORD")
+	dbName := os.Getenv("PG_DB_NAME")
+	port := os.Getenv("PG_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbName, port)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	//err = database.AutoMigrate(&entities.Passenger{}, &entities.Ticket{}, &entities.User{}, &entities.Flight{}, &entities.Reservation{})
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	return database, nil
+}
